8bc4c1deaf20241df7e7138fc920d0114bcab685
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-11-09 16:27:11 +1300

    Added check for R directory, also install dependencies

--- R/dependencies.R
+++ R/dependencies.R
@@ -5,10 +5,15 @@ function(dependency)
 	nz_repo <- 'http://cran.stat.auckland.ac.nz'
 	userdir <- unlist(strsplit(Sys.getenv("R_LIBS_USER"),.Platform$path.sep))[1L]
 
+	if (!file.exists(userdir)) {
+	  if (!dir.create(userdir, recursive = TRUE)) {
+	     stop("Unable to create ", sQuote(userdir))
+	       }
+	 }
 
     # install package
     if(dependency %in% rownames(installed.packages()) == FALSE) {
-               install.packages(dependency, lib=userdir, repos=nz_repo)
+               install.packages(dependency, lib=userdir, repos=nz_repo, dependencies = TRUE)
         } else {
                print(paste("Package already installed: ", dependency))
         }
